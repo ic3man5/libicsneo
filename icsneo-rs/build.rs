1ffe192377e140e16a4481eb537c1360d145def6
David Rebbe <1187684+ic3man5@users.noreply.github.com>
2022-08-19 17:06:22 -0400

    updates to rust build script.
    Signed-off-by: David Rebbe <1187684+ic3man5@users.noreply.github.com>

--- icsneo-rs/build.rs
+++ icsneo-rs/build.rs
@@ -1,15 +1,40 @@
 extern crate bindgen;
 extern crate cmake;
+extern crate path_clean;
 
 use std::env;
 use std::path::PathBuf;
 use cmake::Config;
+use path_clean::clean;
 
 
 fn main() {
+    // Run cmake on libicsneo
     let dst = Config::new("../../libicsneo")
-        .no_build_target(true)
+        .build_target("icsneoc")
+        .define("LIBICSNEO_BUILD_EXAMPLES", "OFF")
+        .define("LIBICSNEO_BUILD_ICSNEOC_STATIC", "OFF")
+        .define("LIBICSNEO_BUILD_ICSNEOLEGACY", "OFF")
         .build();
-    println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=static=libicsneo");
+    // Debug output for lib path
+    println!("cargo:warning=icsneoc.lib search path: {:?}", dst.join("build/Debug").display());
+    // Configure the search path and lib name to link to
+    println!("cargo:rustc-link-search=native={}", dst.join("build/Debug").display());
+    println!("cargo:rustc-link-lib=static=icsneoc");
+
+    // lets generate the bindings
+    let include_path = PathBuf::from(clean(format!("{}/../../include", env!("CARGO_MANIFEST_DIR")).as_str()));
+    println!("cargo:warning=icsneo include path: {:?}", include_path);
+
+    println!("cargo:warning={}", include_path.display());
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .clang_arg(format!("-I{}", include_path.display()))
+        .generate()
+        .expect("Unable to generate bindings");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    println!("cargo:warning=out_path: {:?}", out_path.display());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings");
 }
---
+++ icsneo-rs/wrapper.h
@@ -0,0 +1,1 @@
+#include "icsneo/icsneoc.h"
